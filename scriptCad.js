6c554817dce17a53ab9048bbeb0817450e9c769b
Raphael Burianek <raphael.burianek@gmail.com>
2022-02-14 21:09:26 -0300

    criação da pagina de cadastro e add o script  da senha.

---
+++ scriptCad.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector('.verSenha')
+
+btn.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#senha')
+
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+
+let btnConfirm = document.querySelector('.verConfirmSenha')
+
+btnConfirm.addEventListener('click', () => {
+  let inputConfirmSenha = document.querySelector('#confirmSenha')
+
+  if (inputConfirmSenha.getAttribute('type') == 'password') {
+    inputConfirmSenha.setAttribute('type', 'text')
+  } else {
+    inputConfirmSenha.setAttribute('type', 'password')
+  }
+})
+
